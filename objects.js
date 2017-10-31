57171270e90e25e4130256000d75112714c7f43e
Kevin Randles <krandles@gmail.com>
2017-10-31 09:12:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { radiohead: "Creep" };
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle })
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+}
